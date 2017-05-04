fe800b5ca3dc4aa15d6ecff1e96fd466050587cc
Jordon Moss <mossj32@gmail.com>
2017-05-04 19:47:36 -0300

    Fixed bug in compiler

--- SDMGIT.bat
+++ SDMGIT.bat
@@ -3,13 +3,12 @@ SET PATH=%PATH%;%~dp0\Tools
 set WorkingCopyPath=%~dp0
 set REVISIONNUMBER=Unknown
 
-setlocal enabledelayedexpansion
 for %%k in (HKCU HKLM) do (
     for %%w in (\ \Wow6432Node\) do (
         for /f "skip=2 delims=: tokens=1*" %%a in ('reg query "%%k\SOFTWARE%%wMicrosoft\Windows\CurrentVersion\Uninstall\Git_is1" /v InstallLocation 2^> nul') do (
             for /f "tokens=3" %%z in ("%%a") do (
                 set GIT=%%z:%%b
-                echo Found Git at "!GIT!".
+                echo Found Git at %GIT%.
                 goto GITFOUND
             )
         )
