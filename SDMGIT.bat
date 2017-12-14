26e454fc3d55913f5d9d959a0ab3bf09618a36ac
Jordon Moss <mossj32@gmail.com>
2017-12-13 20:31:12 -0400

    Started a new map. Also, tweaked the SDMGIT.bat.

--- SDMGIT.bat
+++ SDMGIT.bat
@@ -7,23 +7,21 @@ for %%k in (HKCU HKLM) do (
     for %%w in (\ \Wow6432Node\) do (
         for /f "skip=2 delims=: tokens=1*" %%a in ('reg query "%%k\SOFTWARE%%wMicrosoft\Windows\CurrentVersion\Uninstall\Git_is1" /v InstallLocation 2^> nul') do (
             for /f "tokens=3" %%z in ("%%a") do (
-                set GIT=%%z:%%b
+                set "GIT=%%z:%%b"
                 echo Found Git at %GIT%.
-                goto GITFOUND
+				set "PATH=%GIT%bin;%PATH%"
             )
         )
     )
 )
-goto MENU
 
 :GITFOUND
-set PATH=%GIT%bin;%PATH%
 cls
 echo ---------------------------
 echo Retrieving GIT Commit Count
 echo ---------------------------
 for /f "delims=" %%i in ('git rev-list HEAD --count') do ( 
-	set REVISIONNUMBER=%%i
+	set "REVISIONNUMBER=%%i"
 )
 
 :MENU
